20743ca8a58d90ba8a52bf500b14f55043f8ae09
Jhonder Capital <gonzagarciamanuel2008o@gmail.com>
2026-07-18 05:51:22 -0500

    adaptar diseño a celular

--- js/auth.js
+++ js/auth.js
@@ -10,3 +10,39 @@ if (!usuarioActual) {
         "../pages/login.html";
 
 }
+
+const rol = usuarioActual.rol;
+
+if (
+
+    rol === "Analista financiero" ||
+
+    rol === "Gerente General"
+
+) {
+
+    const configuracion =
+        document.querySelector(
+            'a[href="configuracion.html"]'
+        );
+
+    const gastos =
+        document.querySelector(
+            'a[href="gastos.html"]'
+        );
+
+    if (configuracion) {
+
+        configuracion.style.display =
+            "none";
+
+    }
+
+    if (gastos) {
+
+        gastos.style.display =
+            "none";
+
+    }
+
+}
